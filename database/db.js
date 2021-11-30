53c67502482bbacd1b01443c0856f06423b08407
R3WorkZ <reniyal.works@gmail.com>
2021-11-30 21:58:10 +0530

    s

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const database = mongoose.connect(
-  process.env.DB_URL,
+  process.env.DATABASE_URL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
